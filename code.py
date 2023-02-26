e8b74da9da96b5c2f0e26e62ac40ea5a920406a8
richard-feng07 <richardfeng43@gmail.com>
2023-02-26 14:57:34 -0800

    tensor

--- code.py
+++ code.py
@@ -1,1 +1,27 @@
-print("efuif34u")
+from PIL import Image
+from PIL import ImageDraw
+from pycoral.adapters import common
+from pycoral.utils.edgetpu import make_interpreter
+
+_NUM_KEYPOINTS = 17
+
+def main():
+    modelpath = "lite-model_movenet_singlepose_lightning_3.tflite"
+    imagepath = "image.jpg"
+
+    interpreter = make_interpreter(modelpath)
+    interpreter.allocate_tensors()
+
+    img = Image.open(imagepath)
+    resizeimg = img.resize(common.inputsize(interpreter),Image.ANTIALIAS)
+
+    common.set_input(interpreter,resizeimg)
+
+    interpreter.invoke()
+
+    pose = common.output_tensor(interpreter,0).copy().reshape(_NUM_KEYPOINTS,3)
+    print(pose)
+
+
+if __name__ == '__main__':
+    main()
