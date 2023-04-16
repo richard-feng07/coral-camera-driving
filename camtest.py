48aff4b47490abfbf4d4e6eb9de47d053f2d9450
richard-feng07 <richardfeng43@gmail.com>
2023-04-16 15:37:18 -0700

    new

--- camtest.py
+++ camtest.py
@@ -1,6 +1,11 @@
 import cv2
+import gi
+from gi.repository import Gst
 
-cam = cv2.VideoCapture(0)
+Gst.init(None)
+pipeline = Gst.parse_launch("v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480 ! videoconvert ! appsink")
+cam = cv2.VideoCapture()
+cam.open(pipeline)
 _, frame = cam.read()
 cv2.imwrite('photo.jpg',frame)
 cam.release()
