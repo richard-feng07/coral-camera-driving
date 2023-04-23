0b65116aad82ad18ccbafa3e9996ab7298e8bcc7
Jonathan Sahagun <j-sahagun@live.com>
2023-04-23 15:26:30 -0700

    Update server.py

--- server.py
+++ server.py
@@ -5,28 +5,31 @@ from flask import Flask, Response
 
 app = Flask(__name__)
 
-# cap = cv2.VideoCapture(0)
+print('getting cv2.VideoCapture(0)')
+cap = cv2.VideoCapture(0)
+print('got cv2.VideoCapture(0)')
 
-# def gen_frames():
-#     while True:
-#         success, frame = cap.read()
-#         if not success:
-#             break
-#         else:
-#             ret, buffer = cv2.imencode('.jpg', frame)
-#             frame = buffer.tobytes()
-#             yield (b'--frame\r\n'
-#                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+def gen_frames():
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-# @app.route('/video_feed')
-# def video_feed():
-#     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
+print('Starting server')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
